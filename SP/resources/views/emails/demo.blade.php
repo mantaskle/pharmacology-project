c39e9059bc438cc4f4c66c132781dccfb39d7941
satunaite <eglesatunaite@gmail.com>
2019-03-13 01:14:56 +0200

    Fixed login/auth

---
+++ SP/resources/views/emails/demo.blade.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Welcome Email</title>
+</head>
+
+<body>
+<h2>Welcome to the site</h2>
+<br/>
+This is a email demo from 5balloons.info
+</body>
+
+</html>
---
+++ SP/resources/views/emails/welcome.blade.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Registracijos laiškas</title>
+</head>
+
+<body>
+<h2>Sveikiname prisijungus, {{$user['name']}}!</h2>
+<br/>
+Jūsų registracijos el-paštas: {{$user['email']}}.
+</body>
+
+</html>
