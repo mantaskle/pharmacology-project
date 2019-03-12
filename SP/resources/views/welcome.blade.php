9d1f04ef73d84682521058e0de399358767f553f
mantaskle <47858298+mantaskle@users.noreply.github.com>
2019-03-12 22:11:01 +0200

    changed landing page

--- SP/resources/views/welcome.blade.php
+++ SP/resources/views/welcome.blade.php
@@ -11,13 +11,37 @@
 
         <!-- Styles -->
         <style>
+
+
             html, body {
-                background-color: #fff;
-                color: #636b6f;
+                background-image: url("/css/img/background.jpg");
+                background-position: center;
+                background-repeat: no-repeat;
+                background-size: cover;
+                color: #c4c4c4;
                 font-family: 'Nunito', sans-serif;
                 font-weight: 200;
                 height: 100vh;
                 margin: 0;
+            }
+
+            .bg-image
+            {
+            background-image: url("/css/img/background.jpg");
+
+            /* Add the blur effect */
+            filter: blur(8px);
+            -webkit-filter: blur(8px);
+
+            /* Full height */
+            height: 100vh; 
+            top: 0;
+            bottom: 0;
+            z-index: -10;
+            /* Center and scale the image nicely */
+            background-position: center;
+            background-repeat: no-repeat;
+            background-size: cover;
             }
 
             .full-height {
@@ -42,16 +66,18 @@
 
             .content {
                 text-align: center;
+                z-index: 5;
             }
 
             .title {
-                font-size: 84px;
+                font-size: 106px;
+                color: #ffffff;
             }
 
             .links > a {
-                color: #636b6f;
+                color: #cecece;
                 padding: 0 25px;
-                font-size: 13px;
+                font-size: 18px;
                 font-weight: 600;
                 letter-spacing: .1rem;
                 text-decoration: none;
@@ -61,34 +87,50 @@
             .m-b-md {
                 margin-bottom: 30px;
             }
+
+            .bg-text {
+                background-color: rgb(0,0,0); /* Fallback color */
+                background-color: rgba(0,0,0, 0.4); /* Black w/opacity/see-through */
+                border: 3px solid #f1f1f1;
+                position: absolute;
+                top: 50%;
+                left: 50%;
+                transform: translate(-50%, -50%);
+                z-index: 2;
+                width: 40%;
+                padding: 20px;
+                text-align: center;
+            }
         </style>
     </head>
+
     <body>
-        <div class="flex-center position-ref full-height">
-            @if (Route::has('login'))
-                <div class="top-right links">
-                    @auth
-                        <a href="{{ url('/home') }}">Home</a>
-                    @else
-                        <a href="{{ route('login') }}">Login</a>
+            <div class="flex-center position-ref full-height">
+                @if (Route::has('login'))
+                    <div class="top-right links">
+                        @auth
+                            <a href="{{ url('/home') }}">Home</a>
+                        @else
+                            <a href="{{ route('login') }}">Login</a>
 
-                        @if (Route::has('register'))
-                            <a href="{{ route('register') }}">Register</a>
-                        @endif
-                    @endauth
-                </div>
-            @endif
+                            @if (Route::has('register'))
+                                <a href="{{ route('register') }}">Register</a>
+                            @endif
+                        @endauth
+                    </div>
+                @endif
+                <div class="bg-text">
+                    <div class="content">
+                        <div class="title m-b-md">
+                            AMAE
+                        </div>
 
-            <div class="content">
-                <div class="title m-b-md">
-                    Hedghehog
-                </div>
-
-                <div class="links">
-                    <a href="https://hedgehogsandstuff.xyz">Original Page</a>
-                    <a href="https://eziux.xyz">Second Page</a>
-                    <a href="https://github.com/satunaite">GitHub</a>
-                </div>
+                        <div class="links">
+                            <a href="https://hedgehogsandstuff.xyz">Original Page</a>
+                            <a href="https://eziux.xyz">Second Page</a>
+                            <a href="https://github.com/satunaite">GitHub</a>
+                        </div>
+                    </div>
             </div>
         </div>
     </body>
