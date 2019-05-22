10dc66ac85e9393df9696a04a4d657589939780e
satunaite <eglesatunaite@gmail.com>
2019-05-22 09:35:42 +0300

    fixed issue with routes

--- SP/routes/web.php
+++ SP/routes/web.php
@@ -29,31 +29,32 @@ Route::get('/home', 'HomeController@index')->name('home');
 
 Route::middleware(['auth'])->group(function () {
     Route::get('/approval', 'HomeController@approval')->name('approval');    
-    Route::get('/edit_profile', 'HomeController@edit')->name('edit');
-    Route::get('/profile', 'HomeController@profile')->name('profile');
-    Route::get('/vaistu-dozavimas', function () { return view('vaistu-dozavimas');} );    
-    Route::get('/vaistai', function () { return view('vaistai');} );        
-    Route::get('/rezultatai', function () { return view('rezultatai');} );
-    Route::resource('pacients', 'PacientsController');
-    Route::resource('prescriptions', 'PrescriptionsController');
-    Route::resource('medication', 'DosageController');   
-    Route::get('/vaistu-dozavimas', 'DosageController@index');
-    Route::get('/vaistai', 'DosageController@showAll');
-    Route::get('/vaistas', function() { return view('vaistas');} );
-    Route::get('wiki', function() { return view('menu.wiki');} );
-
-    Route::get('event/add','EventController@createEvent');
-    Route::post('event/add','EventController@store');
-    Route::get('event','EventController@calendar');
-
-    Route::get('new_ticket', 'TicketsController@create');
-    Route::post('new_ticket', 'TicketsController@store');
-    Route::get('my_tickets', 'TicketsController@userTickets');
-    Route::get('tickets/{ticket_id}', 'TicketsController@show');
-    Route::post('comment', 'CommentsController@postComment');
-
+    
     Route::middleware(['approved'])->group(function () {
         Route::get('/home', 'HomeController@index')->name('home');
+        Route::get('/edit_profile', 'HomeController@edit')->name('edit');
+        Route::get('/profile', 'HomeController@profile')->name('profile');
+        Route::get('/vaistu-dozavimas', function () { return view('vaistu-dozavimas');} );    
+        Route::get('/vaistai', function () { return view('vaistai');} );        
+        Route::get('/rezultatai', function () { return view('rezultatai');} );
+        Route::resource('pacients', 'PacientsController');
+        Route::resource('prescriptions', 'PrescriptionsController');
+        Route::resource('medication', 'DosageController');   
+        Route::get('/vaistu-dozavimas', 'DosageController@index');
+        Route::get('/vaistai', 'DosageController@showAll');
+        Route::get('/vaistas', function() { return view('vaistas');} );
+        Route::get('wiki', function() { return view('menu.wiki');} );
+
+        Route::get('event/add','EventController@createEvent');
+        Route::post('event/add','EventController@store');
+        Route::get('event','EventController@calendar');
+
+        Route::get('new_ticket', 'TicketsController@create');
+        Route::post('new_ticket', 'TicketsController@store');
+        Route::get('my_tickets', 'TicketsController@userTickets');
+        Route::get('tickets/{ticket_id}', 'TicketsController@show');
+        Route::post('comment', 'CommentsController@postComment');
+
     });
 
     Route::middleware(['admin'])->group(function () {
